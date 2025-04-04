05394e631b30f81dafd620151e3dc166b89260b7
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2025-04-04 02:28:17 -0300

    checking cd in VM

--- src/main/kotlin/app/podrida/controller/HelloController.kt
+++ src/main/kotlin/app/podrida/controller/HelloController.kt
@@ -2,7 +2,9 @@ package app.podrida.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @RequestMapping("/api/hello")
 class HelloController {
     @GetMapping
