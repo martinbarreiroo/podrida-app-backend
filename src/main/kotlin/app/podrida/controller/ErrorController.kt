2e78fff55c07185c9a7f10e9d65268a8494b28c3
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2025-04-05 04:46:53 -0300

    added error controller to check new relic alerts

--- src/main/kotlin/app/podrida/controller/ErrorController.kt
+++ src/main/kotlin/app/podrida/controller/ErrorController.kt
@@ -1,14 +1,15 @@
 package app.podrida.controller
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/hello")
-class HelloController {
+@RequestMapping("/api/error")
+class ErrorController {
     @GetMapping
-    fun hello(): String {
-        return "Hello, just checking if CD works, again!"
+    fun error(): ResponseEntity<String> {
+        return ResponseEntity.status(500).body("This is an error endpoint")
     }
 }
